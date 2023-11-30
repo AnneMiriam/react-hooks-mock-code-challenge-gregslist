f8c5e8bd1579956ba20048f5c81c2cb685b22018
Anne Hastings <anne.miriam@gmail.com>
2023-11-30 11:06:08 -0700

    core deliverables complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+const API = " http://localhost:6001/listings"
+
 function App() {
+  const [listings, setListings] = useState([])
+  const [filter, setFilter] = useState("")
+
+  useEffect(() => {
+    fetch(API)
+    .then(r=>r.json())
+    .then(setListings)
+  }, [])
+
+  function removeListing(id) {
+    setListings(listings.filter(listing => listing.id !== id))
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header  setFilter={setFilter} listings={listings} />
+      <ListingsContainer listings={listings.filter(listing => listing.description.toLowerCase().includes(filter.toLowerCase()))} removeListing={removeListing} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Search() {
+function Search({handleChange, listings}) {
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
@@ -12,8 +12,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={listings.description}
+        onChange={(e) => handleChange(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
